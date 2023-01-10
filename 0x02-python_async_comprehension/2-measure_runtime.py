066700e8620bae9760c5a210991506b516dd2348
kabingusam <kabingusammy@gmail.com>
2023-01-10 13:48:30 +0300

    update on style

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 '''2-measure_runtime.py
 '''
-
 import asyncio
 import time
 
+
 async_comprehension = __import__('1-async_comprehension').async_comprehension
+
 
 async def measure_runtime() -> float:
     '''
@@ -17,3 +18,4 @@ async def measure_runtime() -> float:
 
     get_total = end_time - start_time
     return(get_total)
+    
