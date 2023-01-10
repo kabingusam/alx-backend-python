24e20a46977eb2da3884658b786666f794755cc5
kabingusam <kabingusammy@gmail.com>
2023-01-10 13:44:23 +0300

    task completion

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''2-measure_runtime.py
+'''
+
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+async def measure_runtime() -> float:
+    '''
+    Coroutine that returns the total run time
+    '''
+    start_time = time.perf_counter()
+    get_total = await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end_time = time.perf_counter()
+
+    get_total = end_time - start_time
+    return(get_total)
